7cb21203a109e46059b9103db4301cc85dc26e0a
zgelici <zgelici@gmail.com>
2015-08-24 17:40:35 +0200

    Update install.php

--- install.php
+++ install.php
@@ -80,7 +80,7 @@ if( isset($_GET['install'])) {
             $table->string('ffprobe_path')->default('/usr/local/bin/ffprobe');
             $table->string('webport')->default('8000');
             $table->string('webip');
-            $table->string('hlsfolder')->default('hls');
+            $table->string('hlsfolder')->default('hl');
             $table->timestamps();
         });
 
