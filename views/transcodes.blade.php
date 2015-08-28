510a133e988c28d52cf8f2a5b824a5631cfa402c
zgelici <zgelici@gmail.com>
2015-08-28 02:08:09 +0200

    Update transcodes.blade.php

--- views/transcodes.blade.php
+++ views/transcodes.blade.php
@@ -54,7 +54,7 @@
     @else
         <div class="alert alert-info">
             <button type="button" class="close" data-dismiss="alert">×</button>
-            No admins found
+            No transcodes found
         </div>
     @endif
 @stop
